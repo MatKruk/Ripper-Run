3837e8f6fb014a7ee0e08bd7ec4547bb76ef382a
bethmiles <beth_miles@yahoo.com>
2016-12-07 15:50:21 +0000

    Updated Player
    Added a Woman Victim, added to controller with Talking Anim.
    Added Jack Crouch, Run, Second Attack.
    Added Jack Textures to Player.
    Moved Camera down
    Added Weapon to Hand

--- Group Project/Assets/Scripts/DetectHit.cs
+++ Group Project/Assets/Scripts/DetectHit.cs
@@ -7,11 +7,18 @@ public class DetectHit : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-		
+		if (Input.GetButton("Fire1"))
+		{
 			//Destroy(gameObject);
-		anim.SetBool ("isDead", true);
+			anim.SetBool ("isDead", true);
+		}
+		if (Input.GetButton("Fire2"))
+		{
+			//Destroy(gameObject);
+			anim.SetBool ("isDeadToo", true);
+			print("THIS WORK");
 
-
+		}
     }
 
     // Use this for initialization
