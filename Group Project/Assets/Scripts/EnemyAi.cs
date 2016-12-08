fb2130ae70931357f4440621739d245e13d1ef37
Dan Spurr <13042424@students.southwales.ac.uk>
2016-12-08 17:59:48 +0000

    Improved line of sight
    added a sphere collider so when the player steps into the collider it
    will then check to see if the player can be seen using raycast and
    checking the angle of view

--- Group Project/Assets/Scripts/EnemyAi.cs
+++ Group Project/Assets/Scripts/EnemyAi.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class EnemyAi : MonoBehaviour
 {
 
-    public UnityEngine.AI.NavMeshAgent nav;
-    private Transform player;
-    public GameObject enemy;
-	Animator anim;
-
+	private UnityEngine.AI.NavMeshAgent nav;
+    private Transform playerMove;
+	private GameObject player;
+	private Animator anim;
+	public SphereCollider col; 
     public enum State
     {
         PATROL,
@@ -22,12 +22,12 @@ public class EnemyAi : MonoBehaviour
     //variables for Patrol
     public Transform[] wayPoints;
     private int wayPointId = 0;
-    public float patrolSpeed = 0.5f;
-    public float patrolWaitTime = 1f;
+    public float patrolSpeed = 2.0f;
+    public float patrolWaitTime = 2.0f;
     public float patrolTimer;
 
     // Variables for Chase
-    public float chaseSpeed = 1f;
+    public float chaseSpeed = 2.0f;
 
 
     // Variables for Investigate 
@@ -36,17 +36,23 @@ public class EnemyAi : MonoBehaviour
     public float investigateWait = 10;
 
     // Variables for Sight
-    public float heightMultiplier;
-    public float sightDist = 10;
+	public float fieldOfViewAngle = 110f;
+	public bool canSeePlayer;
+	public Vector3 lastSight;
 
-    public Vector3 lastSight;
-    public bool canSee;
+
+
+	//public float heightMultiplier;
+    //public float sightDist = 10;
+    
+    
 
     void Start()
     {
         nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
 
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerMove = GameObject.FindGameObjectWithTag("Player").transform;
+		player = GameObject.FindGameObjectWithTag("Player");
 
         nav.updatePosition = true;
         nav.updateRotation = true;
@@ -56,7 +62,7 @@ public class EnemyAi : MonoBehaviour
 
         alive = true;
 
-        heightMultiplier = 1.7f;
+        //heightMultiplier = 1.7f;
 
         StartCoroutine("FSM");
     }
@@ -110,11 +116,6 @@ public class EnemyAi : MonoBehaviour
                 patrolTimer = 0;
             }
         }
-
-
-
-
-
     }
 
     void Investigate()
@@ -139,7 +140,7 @@ public class EnemyAi : MonoBehaviour
 
         nav.SetDestination(lastSight);
 
-        if(Vector3.Distance(transform.position, lastSight) <= 3 && !canSee)
+        if(Vector3.Distance(transform.position, lastSight) <= 3 && !canSeePlayer)
         {
             state = EnemyAi.State.INVESTIGATE;
             print("asda");
@@ -147,64 +148,94 @@ public class EnemyAi : MonoBehaviour
 
     }
 
-    void FixedUpdate()
-    {
-        RaycastHit hit;
-        Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, transform.forward * sightDist, Color.blue);
-        Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (transform.forward + transform.right).normalized * sightDist, Color.blue);
-        Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (transform.forward - transform.right).normalized * sightDist, Color.blue);
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject == player) {
+			canSeePlayer = false;
 
-        if (Physics.Raycast(transform.position + Vector3.up * heightMultiplier, transform.forward, out hit, sightDist))
-        {
-            if (hit.collider.gameObject.tag == "Player")
-            {
-                state = EnemyAi.State.CHASE;
-                transform.LookAt(player);
-                lastSight = player.transform.position;
-                canSee = true;
-            }
-            else
-            {
-                canSee = false;
-            }
+			Vector3 direction = other.transform.position - transform.position;
+			float angle = Vector3.Angle (direction, transform.forward);
 
-        }
+			if (angle < fieldOfViewAngle * 0.5f) {
+				RaycastHit hit;
 
-        if (Physics.Raycast(transform.position + Vector3.up * heightMultiplier, (transform.forward + transform.right).normalized, out hit, sightDist))
-        {
-            if (hit.collider.gameObject.tag == "Player")
-            {
-                state = EnemyAi.State.CHASE;
-                transform.LookAt(player);
-                lastSight = player.transform.position;
-                canSee = true;
-            }
-            else
-            {
-                canSee = false;
-            }
+				if (Physics.Raycast (transform.position + transform.up, direction.normalized, out hit, col.radius)) {
+					if (hit.collider.gameObject == player) {
+						canSeePlayer = true;
+						state = EnemyAi.State.CHASE;
+						//transform.LookAt (playerMove);
+						lastSight = player.transform.position;
+					}
+				}
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject == player)
+			canSeePlayer = false;
+	}
 
 
-        }
-
-        if (Physics.Raycast(transform.position + Vector3.up * heightMultiplier, (transform.forward - transform.right).normalized, out hit, sightDist))
-        {
-            if (hit.collider.gameObject.tag == "Player")
-            {
-                state = EnemyAi.State.CHASE;
-                transform.LookAt(player);
-                lastSight = player.transform.position;
-                canSee = true;
-            }
-            else
-            {
-                canSee = false;
-            }
-
-
-
-        }
-
-    }
+   // void FixedUpdate()
+   // {
+   //     RaycastHit hit;
+   //     Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, transform.forward * sightDist, Color.blue);
+   //     Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (transform.forward + transform.right).normalized * sightDist, Color.blue);
+   //     Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (transform.forward - transform.right).normalized * sightDist, Color.blue);
+   //
+   //     if (Physics.Raycast(transform.position + Vector3.up * heightMultiplier, transform.forward, out hit, sightDist))
+   //     {
+   //         if (hit.collider.gameObject.tag == "Player")
+   //         {
+   //             state = EnemyAi.State.CHASE;
+   //             transform.LookAt(player);
+   //             lastSight = player.transform.position;
+   //             canSee = true;
+   //         }
+   //         else
+   //         {
+   //             canSee = false;
+   //         }
+   //
+   //     }
+   //
+   //     if (Physics.Raycast(transform.position + Vector3.up * heightMultiplier, (transform.forward + transform.right).normalized, out hit, sightDist))
+   //     {
+   //         if (hit.collider.gameObject.tag == "Player")
+   //         {
+   //             state = EnemyAi.State.CHASE;
+   //             transform.LookAt(player);
+   //             lastSight = player.transform.position;
+   //             canSee = true;
+   //         }
+   //         else
+   //         {
+   //             canSee = false;
+   //         }
+   //
+   //
+   //     }
+   //
+   //     if (Physics.Raycast(transform.position + Vector3.up * heightMultiplier, (transform.forward - transform.right).normalized, out hit, sightDist))
+   //     {
+   //         if (hit.collider.gameObject.tag == "Player")
+   //         {
+   //             state = EnemyAi.State.CHASE;
+   //             transform.LookAt(player);
+   //             lastSight = player.transform.position;
+   //             canSee = true;
+   //         }
+   //         else
+   //         {
+   //             canSee = false;
+   //         }
+   //
+   //
+   //
+   //     }
+   //
+   // }
 
 }
