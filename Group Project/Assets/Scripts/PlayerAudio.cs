895ebe7b7c9026efba74b4e6e3bf09e3853fddf1
bethmiles <beth_miles@yahoo.com>
2017-02-13 19:02:56 +0000

    Added Player Audio
    Added Jack Footstep when walking using a raycast to each footstep which
    is triggered by the animation. The code also allows for different ground
    types e.g. water and grass. The ground is tagged and is checked to see
    which audio clip to play. This chip is played at random volume.

---
+++ Group Project/Assets/Scripts/PlayerAudio.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour {
+
+	public AudioClip[] stepsounds = new AudioClip[0];
+	public AudioSource source;
+	private Animator anim;
+
+	void Start()
+	{
+		source = GetComponent<AudioSource> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void WalkingFootStepLeft() 
+	{
+		float volume = 1.0f;
+		//float pitch = 1.0f;
+		AudioClip clip = null;
+		RaycastHit hit = new RaycastHit ();
+		if (Physics.Raycast (transform.position, Vector3.down, out hit, 1.5f)) 
+		{
+			string tag = hit.collider.gameObject.tag;
+			if (tag == "Ground") 
+			{
+				clip = stepsounds [0];
+				volume = UnityEngine.Random.Range (0.04f, 0.08f);
+				print ("Ground: Audio Play");
+			} 
+			else if (tag == "Water") 
+			{
+				clip = stepsounds [1];
+				volume = UnityEngine.Random.Range (0.03f, 0.08f);
+				print ("Water: Audio Play");
+			} 
+			else if (tag == "Grass") 
+			{
+				clip = stepsounds [2];
+				volume = UnityEngine.Random.Range (0.03f, 0.08f);
+				print ("Grass: Audio Play");
+			}
+		}
+		if (clip != null) 
+		{
+			source.PlayOneShot (clip, volume);
+			//source.pitch = UnityEngine.Random.Range(0.0f, 0.6f);
+		}
+
+	}
+
+	void WalkingFootStepRight() 
+	{
+		float volume = 1.0f;
+		//float pitch = 1.0f;
+		AudioClip clip = null;
+		RaycastHit hit = new RaycastHit ();
+		if (Physics.Raycast (transform.position, Vector3.down, out hit, 1.5f)) 
+		{
+			string tag = hit.collider.gameObject.tag;
+			if (tag == "Ground") 
+			{
+				clip = stepsounds [0];
+				volume = UnityEngine.Random.Range (0.03f, 0.08f);
+				print ("Ground: Audio Play");
+			} 
+			else if (tag == "Water") 
+			{
+				clip = stepsounds [1];
+				volume = UnityEngine.Random.Range (0.03f, 0.08f);
+				print ("Water: Audio Play");
+			}
+			else if (tag == "Grass") 
+			{
+				clip = stepsounds [2];
+				volume = UnityEngine.Random.Range (0.03f, 0.08f);
+				print ("Grass: Audio Play");
+			}
+		}
+		if (clip != null) 
+		{
+			source.PlayOneShot (clip, volume);
+		}
+
+	}
+
+
+}
