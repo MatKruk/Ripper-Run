6097cb75d090e9dd1712cf6b93c8492d89f59096
Mat Kruk <13000985@students.southwales.ac.uk>
2017-03-03 18:32:38 +0000

    Added Occlusion Culling + further map development

---
+++ Group Project/Assets/Scripts/FollowPlayerParticle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerParticle : MonoBehaviour {
+
+
+	public Transform Player;
+	private GameObject PlayerRotation;
+
+	void FixedUpdate () {
+
+		this.transform.position = new Vector3 (Player.position.x, this.transform.position.y, Player.position.z);
+		transform.eulerAngles = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+
+	}
+}
