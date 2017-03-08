85af051905972a18b76eec7493a0a06eac66031a
bethmiles <beth_miles@yahoo.com>
2017-03-08 12:44:57 +0000

    Fix error to Win Hideout, police triggereing end

--- Group Project/Assets/Scripts/Win.cs
+++ Group Project/Assets/Scripts/Win.cs
@@ -17,12 +17,14 @@ public class Win : MonoBehaviour
 
 	void OnTriggerEnter(Collider other)
 	{
-		if (victim.isDead == true) 
-		{
-			GameObject.Find("Jack").SendMessage("finish");
-			SceneManager.LoadScene ("Win_Scene", LoadSceneMode.Single);
-			//print ("This works");
-		}
+		if (other.tag == "Player") {
+			if (victim.isDead == true) {
+				GameObject.Find ("Jack").SendMessage ("finish");
+				SceneManager.LoadScene ("Win_Scene", LoadSceneMode.Single);
+				//print ("This works");
+			}
+		} //else
+			//Debug.Log ("Not Player: " + other.name);
 	}
 }
 
