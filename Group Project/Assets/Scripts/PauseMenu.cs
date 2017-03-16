8e7f4b3c8848ee9210633b3db0db7d8933e8a81c
bethmiles <beth_miles@yahoo.com>
2017-03-16 12:50:35 +0000

    Pause Menu
    Added a Pause menu when escape is pressed, allows the player to resume,
    quit, acess video and sound settings.

---
+++ Group Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour {
+
+	public Transform pause;
+	public Transform camera;
+	public string loadLevel;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			Pause ();
+		}
+
+	}
+
+	public void Pause()
+	{
+		if (pause.gameObject.activeInHierarchy == false) 
+		{
+			pause.gameObject.SetActive (true);
+			Time.timeScale = 0;
+			camera.GetComponent<CameraMouseLook> ().enabled = false;
+		} 
+		else 
+		{
+			pause.gameObject.SetActive (false);
+			Time.timeScale = 1;
+			camera.GetComponent<CameraMouseLook> ().enabled = true;
+		}
+	}
+
+	public void Quit()
+	{
+		Time.timeScale = 1;
+		SceneManager.LoadScene (loadLevel);
+		//Application.Quit();
+	}
+}
