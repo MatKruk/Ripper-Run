79ea25c156cb3d2ff5bc27e50b19c948f2264a62
SpaceBiker <13003429@students.southwales.ac.uk>
2017-04-29 01:56:31 +0100

    Added Switch script
    Added a script that switches between the player controlls and the horse
    controls
    
    Fixed the problem I was having when I tried before.

--- Group Project/Assets/Scripts/Carriage Scripts/HorseControl.cs
+++ Group Project/Assets/Scripts/Carriage Scripts/HorseControl.cs
@@ -3,6 +3,45 @@ using System.Collections;
 
 public class HorseControl : MonoBehaviour {
 
+	//Better
+	private Rigidbody rb;
+	//movement speed
+	public float speed = 2.0f;
+	//turn speed
+	public float tSpeed = 5.0f;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent <Rigidbody> ();
+	}
+
+
+	void FixedUpdate () 
+	{
+		/*float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);*/
+
+		float vert = Input.GetAxis("Vertical") * speed;
+		float horiz = Input.GetAxis("Horizontal") * speed;
+
+		//Keep movements smooth
+		vert *= Time.deltaTime;
+		horiz *= Time.deltaTime;
+
+		transform.Translate(0, 0, vert * speed);
+		transform.Rotate (0, horiz * tSpeed, 0);
+
+		//rb.AddForce (0, 0, vert * speed);
+		//rb.AddTorque (0, horiz * speed, 0);
+
+	}
+
+	/* OLD
+	 
 	public float speed = 20f;
 	public float turnSpeed = 5f;
 
@@ -31,6 +70,6 @@ public class HorseControl : MonoBehaviour {
 						  
 		horseRigidbody.AddTorque (0f, turnInput * turnSpeed, 0f);
 
-	}
+	}*/
 
 }
---
+++ Group Project/Assets/Scripts/Carriage Scripts/Switch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour 
+{
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		/*GetComponent<PlayerCamera> ().enabled = true;
+		//GetComponent<MoveTo> ().enabled = true;
+		//GetComponent<HorseControl> ().enabled = false;*/
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown ("r")/*trigger box*/) 
+		{
+			//Find the Horse object that is found under Carriage
+			GameObject disable = GameObject.Find("Carriage/Horse");
+			//GameObject hcScript = GameObject.Find("Carriage/Horse");
+			//GameObject dNav = GameObject.Find ("Carriage/Horse");
+
+			//Disable player (Jack's) controls
+			GetComponent<PlayerCamera> ().enabled = false;
+			//Disable the MoveTo Script that is attached to the Horse object
+			disable.GetComponent<MoveTo>().enabled = false;
+			disable.GetComponent<HorseControl> ().enabled = true;
+
+			//disable the NavMeshAgent as it interferes with the player controlled horse
+			disable.GetComponent <UnityEngine.AI.NavMeshAgent>().enabled = false;
+		}
+
+			
+	}
+}
+
