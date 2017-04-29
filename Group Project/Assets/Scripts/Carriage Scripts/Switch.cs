04499e5a9935c38b5bb408f5368f0f32c4de45c7
SpaceBiker <13003429@students.southwales.ac.uk>
2017-04-29 17:47:22 +0100

    Added camera switch
    The Camera on Jack deactivates and the camera on the horse activates.

--- Group Project/Assets/Scripts/Carriage Scripts/Switch.cs
+++ Group Project/Assets/Scripts/Carriage Scripts/Switch.cs
@@ -6,32 +6,41 @@ public class Switch : MonoBehaviour
 {
 
 
+
 	// Use this for initialization
 	void Start () 
 	{
-		/*GetComponent<PlayerCamera> ().enabled = true;
-		//GetComponent<MoveTo> ().enabled = true;
-		//GetComponent<HorseControl> ().enabled = false;*/
+
 
 	}
 
 
 	void OnTriggerEnter (Collider other) 
 	{
+		
 		//If the player presses r while in the trigger box they will control the horse
 		if (other.gameObject.CompareTag("Trigger") && Input.GetKeyDown ("r")) 
 		{
+
+			//make it switch back on same button press
+			//move player to location i.e. GameObject Jack
+
 			//Find the Horse object that is found under Carriage
 			GameObject disable = GameObject.Find("Carriage/Horse");
-			//GameObject hcScript = GameObject.Find("Carriage/Horse");
-			//GameObject dNav = GameObject.Find ("Carriage/Horse");
+			//Find the HCamera Object that is found under Horse
+			GameObject Hcamera = GameObject.Find("Carriage/Horse/HCamera");
+			//Find the MainCamera that is on Jack
+			GameObject Pcamera = GameObject.Find("Main Camera"); 
 
-			//Disable player (Jack's) controls
+			Pcamera.GetComponent<Camera>().enabled = false; 
+			Hcamera.GetComponent<Camera> ().enabled = true;
+
+
+			//Disable player (Jack's) control script
 			GetComponent<PlayerCamera> ().enabled = false;
 			//Disable the MoveTo Script that is attached to the Horse object and enable the horse controls
 			disable.GetComponent<MoveTo>().enabled = false;
 			disable.GetComponent<HorseControl> ().enabled = true;
-
 			//disable the NavMeshAgent as it interferes with the player controlled horse
 			disable.GetComponent <UnityEngine.AI.NavMeshAgent>().enabled = false;
 		}
